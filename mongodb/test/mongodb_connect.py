3ada4fce087064ad5690f52f1aa0783cfc23d522
jason19970210 <jason19970210@gmail.com>
2022-10-04 08:46:31 +0800

    update connect test script

--- mongodb/test/mongodb_connect.py
+++ mongodb/test/mongodb_connect.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv()
-
-MONGODB_IP = os.getenv("MONGODB_IP")
+MONGODB_HOST = os.getenv("MONGODB_HOST")
 MONGODB_PORT = int(os.getenv("MONGODB_PORT"))
 MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
 MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
@@ -12,7 +11,7 @@ MONGODB_DBNAME = os.getenv("MONGODB_DBNAME")
 
 client = MongoClient()
 
-client = MongoClient(f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_IP}:{MONGODB_PORT}/?authMechanism=DEFAULT&authSource={MONGODB_DBNAME}")
+client = MongoClient(f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_HOST}:{MONGODB_PORT}/?authMechanism=DEFAULT&authSource={MONGODB_DBNAME}")
 
 db = client[MONGODB_DBNAME]
 
